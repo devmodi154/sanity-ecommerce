c22fb3204ad1fbfdb683314e0a58d85dd4e8e851
Devanshu Modi <devmodi154@gmail.com>
2022-07-31 15:32:50 +0530

    First commit, stripe payment not working

---
+++ context/StateContext.js
@@ -0,0 +1,98 @@
+import React, { createContext, useContext, useState} from 'react'
+import toast from 'react-hot-toast';
+
+const Context = createContext();
+
+export const StateContext = ( { children }) => {
+    const [showCart, setShowCart] = useState(false);
+    const [cartItems, setCartItems] = useState([]);
+    const [totalPrice, setTotalPrice] = useState(0);
+    const [totalQuantities, setTotalQuantities] = useState(0);
+    const [qty, setQty] = useState(1);
+    let foundProduct, index;
+
+    const incQty =  () => {
+        setQty((prevQty) => prevQty + 1);
+    }
+
+    const decQty =  () => {
+        setQty((prevQty) => {
+            if (prevQty - 1 < 1) return 1;
+            return prevQty - 1;
+        });
+    }
+
+    const onAdd = (product, quantity) => {
+        const checkProductionInCart = cartItems.find((item) => item._id === product._id);
+        
+        setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price*quantity);
+        setTotalQuantities((prevTotalQuanitites) => prevTotalQuanitites + quantity);
+
+        if (checkProductionInCart) {
+            const updateCartItems = cartItems.map((cartProduct) => {
+                if (cartProduct._id === product._id) return {
+                    ...cartProduct,
+                    quantity: cartProduct.quantity + quantity
+                }
+            })
+
+            setCartItems(updateCartItems);
+        } else {
+            product.quantity = quantity;
+            setCartItems([...cartItems, { ...product}]);
+        }
+        toast.success(`${qty} ${product.name} added to the cart.`);
+    }
+
+    const onRemove = (product) => {
+        foundProduct = cartItems.find((item) => item._id  === product._id);
+        const newCartItems = cartItems.filter((item) => item._id !== product._id );
+        setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price * foundProduct.quantity);
+        setTotalQuantities((prevTotalQuanitites) => prevTotalQuanitites - foundProduct.quantity);
+        setCartItems(newCartItems);
+    }
+
+    const toggleCartItemQuantity = (id, value) => {
+        foundProduct = cartItems.find((item) => item._id  === id);
+        index = cartItems.findIndex((product) => product._id === id);
+        const newCartItems = cartItems.filter((item) => item._id !== id )
+
+        if (value === 'inc') {
+            setCartItems([...newCartItems, { ... foundProduct, quantity: foundProduct.quantity + 1}]);
+            setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price);
+            setTotalQuantities((prevTotalQuanitites) => prevTotalQuanitites + 1);
+        } else if (value === 'dec') {
+            if (foundProduct.quantity > 1) {
+                setCartItems([...newCartItems, { ... foundProduct, quantity: foundProduct.quantity - 1}]);
+                setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price);
+                setTotalQuantities((prevTotalQuanitites) => prevTotalQuanitites - 1);
+            }
+        }
+
+    }
+
+    return (
+        <Context.Provider
+            value={{
+                showCart,
+                cartItems,
+                totalPrice,
+                totalQuantities,
+                qty, 
+                incQty,
+                decQty,
+                onAdd,
+                setShowCart,
+                setCartItems,
+                setTotalPrice,
+                setTotalQuantities,
+                toggleCartItemQuantity,
+                onRemove
+            }}
+        >
+            {children}
+        </Context.Provider>
+    )
+}
+
+export const useStateContext = () => useContext(Context);
---
+++ pages/api/stripe.js
@@ -0,0 +1,51 @@
+import Stripe from 'stripe';
+
+const stripe = new Stripe(`${sk_test_51LRIaeSEQIJXJnrj6LKFN1JO3exgKZ6obL6TVpBrT358XkN9ZLzebcJnXcR7HzM6nqzrhw0zqVkIAF6KUNmgnrA500vOjQz5Fj}`);
+
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const params = {
+        submit_type: 'pay',
+        mode: 'payment',
+        payment_method_types: ['card'],
+        billing_address_collection: 'auto',
+        shipping_options: [
+          { shipping_rate: 'shr_1Kn3IaEnylLNWUqj5rqhg9oV' },
+        ],
+        line_items: req.body.map((item) => {
+          const img = item.image[0].asset._ref;
+          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/vfxfwnaw/production/').replace('-webp', '.webp');
+
+          return {
+            price_data: { 
+              currency: 'usd',
+              product_data: { 
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled:true,
+              minimum: 1,
+            },
+            quantity: item.quantity
+          }
+        }),
+        success_url: `${req.headers.origin}/success`,
+        cancel_url: `${req.headers.origin}/canceled`,
+      }
+
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(params);
+
+      res.status(200).json(session);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
+}
